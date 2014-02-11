d33f0ba23c15c0afc996200d0efbb263f9c9b523
Nelsol Batalla <nelsolbatalla@Nelsol-Batallas-MacBook-Pro.local>
2014-02-11 16:41:52 -0600

    made changes

---
+++ source/articles/2014-02-11-another-example-article.html.markdown
@@ -0,0 +1,8 @@
+---
+title: How To Estimtate UX Projects
+date: 2014-02-11
+category: example
+image: test-image.jpg
+---
+
+This is yet another post! Hello world!
--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,3 +1,7 @@
 $(function() {
-	
+	var titleHeight = parseFloat($('.latest-post h2').css('height'));
+	var titleHeightHalf = titleHeight / 2;
+	console.log(titleHeightHalf);
+
+	$('.latest-post h2').css('margin-top', -titleHeightHalf);
 });
