e02df8c5b49a0febb50391161ea9895514ef2ab2
Nelsol Batalla <nelsolbatalla@Nelsol-Batallas-MacBook-Pro.local>
2014-02-11 21:19:49 -0600

    modified config

--- config.rb
+++ config.rb
@@ -105,7 +105,7 @@ end
 activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
-  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
+  deploy.remote   = "blog" # remote name or git url, default: origin
   # deploy.branch   = "custom-branch" # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
 end
