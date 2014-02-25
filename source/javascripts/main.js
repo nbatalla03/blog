371afb76d9a3026e77a9c992a50fefa894d65aef
nbatalla03 <n.batalla03@gmail.com>
2014-02-24 21:00:32 -0600

    commented out javascript

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,23 +1,5 @@
 $(function() {
-#	var titleHeight = parseFloat($('.latest-post h2').css('height'));
-	var titleHeightHalf = titleHeight / 2;
-	console.log(titleHeightHalf);
-
-	$('.latest-post h2').css('margin-top', -titleHeightHalf);
-
-	var img = new Image();
-	img.onload = function() {
-		var blendImg = new Image();
-		blendImg.onload = function() {
-			Pixastic.process(img, "blend", 
-				{
-					amount : 1, 
-					mode : "multiply", 
-					image : blendImg
-				}
-			);
-		}
-		blendImg.src = "header-logo.png";
-	}
-	img.src = "myimage.jpg";
+  // $('.latest-post ul li').wookmark({
+  //   container: $('.latest-post')
+  // });
 });
